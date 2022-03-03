2b74f4a6d482ad85848551f161ed249f1d78ef3c
swap123 <misalswapnil77@gmail.com>
2022-03-03 15:27:34 +0530

    Geometry

---
+++ src/com/bridgelabz/Geometry.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class Geometry {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line commarision and Commutation Program");
+    }
+}
