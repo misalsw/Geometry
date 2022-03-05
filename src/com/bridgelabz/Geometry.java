d93ab0bd55b3b6b3b5f14fca97e35a60c87273d7
swap123 <misalswapnil77@gmail.com>
2022-03-05 13:25:04 +0530

    Comparing Two Lines and Resulting Accordingly

--- src/com/bridgelabz/Geometry.java
+++ src/com/bridgelabz/Geometry.java
@@ -1,38 +1,77 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
-public class Geometry {
+class Geometry {
+
+    public double equals(double x3, double y3, double x4, double y4, double length) {
+        System.out.println("Comparing two Lines and Resulting Accordingly");
+
+        double length2 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.sqrt(Math.pow((y4 - y3), 2)));
+
+        if (length == length2) {
+            System.out.println("The Two lines are Equal");
+        } else {
+            System.out.println("The Two lines are not Equal");
+        }
+        return length2;
+    }
+    public void compareTo (double length, double length2) {
+
+        if (length == length2) {
+            System.out.println("The Two lines are Equal in Length");
+        }
+
+        else if (length > length2) {
+            System.out.println("The First line is Greater");
+        }
+
+        else {
+            System.out.println("The Second line is Greater");
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Calculate the Length");
-        int neg =(int)Math.floor(Math.random() * 2);
-        int x1 = (int)Math.floor(Math.random() * 10);
 
-        if (neg == 1) {
-            x1 *= -1;  //it combines the result of combination  
-        }
+        Geometry obj = new Geometry();
+        Scanner read = new Scanner(System.in);
 
-        neg = (int)Math.floor(Math.random() * 2);
-        int y1 =(int)Math.floor(Math.random() * 10);
-        if (neg == 1) {
-            y1 *= -1;
-        }
+        System.out.print("Enter x1: ");
+        double x1 = read.nextDouble();
 
-        neg =(int) Math.floor(Math.random() * 2);
-        int x2 =(int)Math.floor(Math.random() * 10);
-        if (neg == 1) {
-            x2 *= -1;
-        }
+        System.out.print("Enter y1: ");
+        double y1 = read.nextDouble();
 
-        neg =(int) Math.floor(Math.random() * 2);
-        int y2 = (int)Math.floor(Math.random() * 10);
-        if (neg == 1) {
-            y2 *= -1;
-        }
+        System.out.print("Enter x2: ");
+        double x2 = read.nextDouble();
 
-        int length = (int)Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.print("Enter y2: ");
+        double y2 = read.nextDouble();
+
+        System.out.print("Enter x3: ");
+        double x3 = read.nextDouble();
+
+        System.out.print("Enter y3: ");
+        double y3 = read.nextDouble();
+
+        System.out.print("Enter x4: ");
+        double x4 = read.nextDouble();
+
+        System.out.print("Enter y4: ");
+        double y4 = read.nextDouble();
+
+        double length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 -y1), 2));
 
         System.out.println(" The first point is : (" + x1 + ", " + y1 + ")");
-        System.out.println(" The Second point is : (" + x2 + ", " + y2 + ")");
-        System.out.println(" The length of the line is : " + length);
+        System.out.println(" The second point is : (" + x2 + ", " + y2 + ")");
 
+        System.out.println(" The third point is : (" + x3 + ", " + y3 + ")");
+        System.out.println(" The fourth point is : (" + x4 + ", " + y4 + ")");
+
+         double length2 = obj.equals(x3 , y3, x4, y4, length);
+        System.out.println(" The length of the line is : " + length);
+        System.out.println(" Second line length: " + length2);
+
+       obj.compareTo (length , length2);
     }
 }
